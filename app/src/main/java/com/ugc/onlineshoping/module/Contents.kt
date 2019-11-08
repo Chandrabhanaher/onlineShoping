433c56b10cc02f37b2b91fae69690d63f2c97830
ChandrabhanAher <53216031+Chandrabhanaher@users.noreply.github.com>
2019-11-08 14:26:03 +0530

    Update Contents.kt

--- app/src/main/java/com/ugc/onlineshoping/module/Contents.kt
+++ app/src/main/java/com/ugc/onlineshoping/module/Contents.kt
@@ -1,5 +1,5 @@
 package com.ugc.onlineshoping.module
 
 object Contents {
-    var SHOE_URL:String ="http://14.143.175.253:8080/online_shopping/"
+    var SHOE_URL:String ="http://localhost/online_shopping/"
 }
